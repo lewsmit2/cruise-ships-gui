cef511b32f6b96c6a9c1e469e162407e95c9f0ce
Lewis Smith <lewislbssmith212@gmail.com>
2022-12-27 13:41:45 +0000

    add and remove ships to port

--- src/Port.js
+++ src/Port.js
@@ -1,7 +1,22 @@
 class Port {
     constructor(name){
         this.name = name;
+        this.ships = [];
     };
+
+    addShip(ship){
+       this.ships.push(ship); 
+    };
+
+    removeShip(ship){
+        const shipIndex = this.ships.indexOf(ship);
+        if(shipIndex >= 0){
+            this.ships.splice(shipIndex, 1);
+        } else {
+            throw new Error('No ships currently at dock');
+        }
+    };
+
 }
 
 
