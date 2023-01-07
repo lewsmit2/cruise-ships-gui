850639e5dd3149641146cc6eccd7512ab3e98216
Lewis Smith <lewislbssmith212@gmail.com>
2023-01-07 15:12:21 +0000

    IIFEs on Constructor Functions to demarcate window object and node.js environment

--- src/Port.js
+++ src/Port.js
@@ -1,21 +1,26 @@
-class Port {
-  constructor(name) {
-    this.name = name;
-    this.ships = [];
-  }
+(function exportPort() {
+  class Port {
+    constructor(name) {
+      this.name = name;
+      this.ships = [];
+    }
 
-  addShip(ship) {
-    this.ships.push(ship);
-  }
+    addShip(ship) {
+      this.ships.push(ship);
+    }
 
-  removeShip(ship) {
-    const shipIndex = this.ships.indexOf(ship);
-    if (shipIndex >= 0) {
-      this.ships.splice(shipIndex, 1);
-    } else {
-      throw new Error('No ships currently at dock');
+    removeShip(ship) {
+      const shipIndex = this.ships.indexOf(ship);
+      if (shipIndex >= 0) {
+        this.ships.splice(shipIndex, 1);
+      } else {
+        throw new Error('No ships currently at dock');
+      }
     }
   }
-}
-
-module.exports = Port;
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = Port;
+  } else {
+    window.Port = Port;
+  }
+}());
