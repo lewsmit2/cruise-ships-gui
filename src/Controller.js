518952f3f2aa53276f77c62e1dce94a17da2c56c
Lewis Smith <lewislbssmith212@gmail.com>
2023-01-07 16:16:35 +0000

    rendering ports to html; adding forEach onto prototype method in port

--- src/Controller.js
+++ src/Controller.js
@@ -15,6 +15,23 @@
         backgroundIndex += 1;
       }, 1000);
     }
+
+    renderPorts(ports) {
+      const portsElement = document.querySelector('#ports');
+      portsElement.style.width = '0px';
+
+      ports.forEach((port, index) => {
+        const newPortElement = document.createElement('div');
+        newPortElement.dataset.portName = port.Name;
+        newPortElement.dataset.portIndex = index;
+        newPortElement.className = 'port';
+
+        portsElement.appendChild(newPortElement);
+
+        const portsElementWidth = parseInt(portsElement.style.width, 10);
+        portsElement.style.width = `${portsElementWidth + 256}px`;
+      });
+    }
   }
   if (typeof module !== 'undefined' && module.exports) {
     module.exports = Controller;
